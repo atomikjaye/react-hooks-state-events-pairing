f6f19fb116a4dca14e73deace7588a0ae0cb2885
Jocelyn Naarden <tzionone@gmail.com>
2022-06-02 00:43:27 -0400

    Got a basic one pager done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,45 @@
 import video from "../data/video.js";
 
+function Header({ title }) {
+  return <h1>{title}</h1>
+}
+
+function VideoStats({ views, createdAt }) {
+  return (
+    <div>
+      <p>{views} Views | Uploaded {createdAt}</p>
+    </div>
+  )
+}
+
+function VideoVotes({ upvotes, downvotes }) {
+  return (
+    <div>
+      <button>{upvotes}👍🏽</button>
+      <button>{downvotes}👎🏽</button>
+    </div>
+  )
+}
+
+function CommentsDisplay({ comments }) {
+  const displayComments = comments.map((comment) => {
+    return (
+      <Comment user={comment.user} comment={comment.comment} />
+    )
+  })
+  return displayComments
+}
+
+function Comment({ user, comment }) {
+  return (
+    <div>
+      <h3>{user}</h3>
+      <p>{comment}</p>
+    </div>
+  )
+}
+
+
 function App() {
   console.log("Here's your data:", video);
 
@@ -13,6 +53,11 @@ function App() {
         allowFullScreen
         title="Thinking in React"
       />
+      <Header title={video.title} />
+      <VideoStats views={video.views} createdAt={video.createdAt} />
+      <VideoVotes upvotes={video.upvotes} downvotes={video.downvotes} />
+      <button>Hide Comments</button>
+      <CommentsDisplay comments={video.comments} />
     </div>
   );
 }
